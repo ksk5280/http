9b2d0e67aff517d182730808259f4d1cecb1348c
Kimiko Kano <kskano@gmail.com>
2016-01-04 14:44:03 -0700

    first test

--- test/server_test.rb
+++ test/server_test.rb
@@ -1,1 +1,13 @@
 require 'test_helper'
+require 'hurley'
+require 'minitest'
+require 'server'
+
+class ServerTest < Minitest::Test
+  def test_listens_to_port_9292
+    response = Hurley.get("http://127.0.0.1:9292")
+    assert_equal response.body, "<http><head></head><body>Hello, World! (6)</body></html>"
+  end
+
+  
+end
