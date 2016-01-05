91afd557220787c0802b363f417a798672a9b2f4
Kimiko Kano <kskano@gmail.com>
2016-01-04 22:11:40 -0700

    parsing request

--- lib/server.rb
+++ lib/server.rb
@@ -1,7 +1,13 @@
 require 'socket'
+require 'hurley'
 
 class Server
-  attr_reader :tcp_server, :client, :request_count, :request_lines
+  attr_reader :tcp_server,
+              :client,
+              :request_count,
+              :request_lines,
+              :request_details
+
   def initialize
     @tcp_server = TCPServer.new(9292)
     @request_count = 0
@@ -16,17 +22,15 @@ class Server
     end
     puts "Got this request:"
     puts request_lines.inspect
+    require 'pry'
+    binding.pry
     @request_count += 1
     send_response
     client.close
   end
 
   def send_response
-    #parse the path
-    path = request_lines[0].split[1]
-    puts path
-    #generate the response string
-    #send_this output
+    #parse the request lines
     response = "<pre>" + request_lines.join("\n") + "</pre>"
     output = "<http><head></head><body>#{response}</body></html>"
     client.puts headers(output)
@@ -41,6 +45,16 @@ class Server
       "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   end
 
+  def parse_request
+    @request_details = {}
+    request_details["Verb"] = request_lines[0].split[0]
+    request_details["Path"] = request_lines[0].split[1]
+    request_details["Protocol"] = request_lines[0].split[2]
+    request_details["Host"] = request_lines[1].split(":")[1]
+    request_details["Port"] = request_lines[1].split(":")[2]
+    request_details["Origin"] = "I have no idea"
+    request_details["Accept"] = ""
+  end
 end
 
 if __FILE__ == $0
