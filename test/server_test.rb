420d2d3605ec31992d3b708a18e1fec79749a08a
Erinna Chen <erinna.chen@gmail.com>
2016-01-04 21:26:30 -0700

    Added tests for iteration 0; reverted response back to iteration 0

--- test/server_test.rb
+++ test/server_test.rb
@@ -9,15 +9,20 @@ class ServerTest < Minitest::Test
     @hclient = Hurley::Client.new "http://127.0.0.1:9292"
   end
 
-  def test_returns_calling_the_correct_host
+  def test_can_request_from_server
     response = hclient.get("/")
-    assert_equal response.header[:server], "ruby"
+    assert response.success?
   end
 
-  def test_responds_to_different_paths
-    response = hclient.get("/")
-    assert_equal hclient.port, 9292
+  def test_hello_world_increments
+    response = hclient.get("/hello")
+    hw_num = response.body[/\d+/].to_i
+    hw_string = "<http><head></head><body>Hello, World! (#{hw_num})</body></html>"
+    assert_equal hw_string, response.body
+    hw_num += 1
+    hw_string = "<http><head></head><body>Hello, World! (#{hw_num})</body></html>"
+    response = hclient.get("/hello")
+    assert_equal hw_string, response.body
   end
-
 
 end
